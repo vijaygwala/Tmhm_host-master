7d7052e515d4d6c3a131e2c79872d328349851cf
tmhmindia <tmhmindia@gmail.com>
2020-07-19 14:44:38 +0530

    added ip address

--- tmhm_host/settings.py
+++ tmhm_host/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '5w^7@844)#j4!=w*8_sd_mr(0!q42n3@3@)gab91r!opc32t^9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['128.199.219.129', '127.0.0.1']
+ALLOWED_HOSTS = ['139.59.65.7', '128.199.219.129', '127.0.0.1']
 
 # Application definition
 
