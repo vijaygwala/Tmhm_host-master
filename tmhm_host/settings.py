4bac48aa3c2250cebfc090af91429c87e6460b00
Ndeepanshu <05natsudragneel18@gmail.com>
2020-07-24 17:47:15 +0530

    settings.py updated

--- tmhm_host/settings.py
+++ tmhm_host/settings.py
@@ -31,16 +31,13 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 SECRET_KEY = '5w^7@844)#j4!=w*8_sd_mr(0!q42n3@3@)gab91r!opc32t^9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-<<<<<<< HEAD
+
 #DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.learnopad.com', 'learnopad.com', '68.183.81.120', '127.0.0.1']
-=======
-DEBUG = True
+ALLOWED_HOSTS = ['www.learnopad.com', 'learnopad.com', '68.183.81.120', '139.59.42.126', '127.0.0.1']
 
-ALLOWED_HOSTS = ['128.199.219.129', '127.0.0.1']
->>>>>>> backend
+
 
 # Application definition
 
@@ -67,11 +64,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'widget_tweaks',
     'myauth',
-<<<<<<< HEAD
-=======
+
     'rest_framework'
->>>>>>> backend
-    # endVijay
+
     
 
 ]
@@ -129,7 +124,7 @@ WSGI_APPLICATION = 'tmhm_host.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-<<<<<<< HEAD
+
 if DEBUG:
     DATABASES = {
         'default': {
@@ -148,27 +143,7 @@ else:
             'PORT': '',
         }
     }
-=======
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mydb',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'dbadmin',
-        'PASSWORD': 'digitalTmhm',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}'''
->>>>>>> backend
 
 
 # Password validation
