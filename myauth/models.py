90c20222fa724273b2fb56001128d4db927f34fd
Vijay gwala <55628967+vijaygwala@users.noreply.github.com>
2020-08-10 11:24:27 +0530

    code merged successfully

--- myauth/models.py
+++ myauth/models.py
@@ -71,9 +71,9 @@ class Profile(models.Model):
     def __str__(self):  # __unicode__ for Python 2
         return self.user.email
 
-@receiver(post_save, sender=CustomUser)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-        #Token.objects.create(user=instance)
-    instance.profile.save()
+# @receiver(post_save, sender=CustomUser)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#         #Token.objects.create(user=instance)
+#     instance.profile.save()
