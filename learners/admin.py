2e0b80d3178665999749c966d6e5925cbae1e513
Vijay gwala <55628967+vijaygwala@users.noreply.github.com>
2020-08-11 01:05:54 +0530

    model learners

--- learners/admin.py
+++ learners/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class LearnersAdmin(admin.ModelAdmin):
+    list_display=('Lid','name','DOB','phone','status','user')
+
+admin.site.register(Learners,LearnersAdmin)
