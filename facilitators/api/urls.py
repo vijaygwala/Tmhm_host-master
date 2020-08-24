fd6249fb3738e73a545d5ed6951fd1cbef7f9b48
todi-2000 <manshitodi@gmail.com>
2020-08-25 01:12:04 +0530

    Front-end

--- facilitators/api/urls.py
+++ facilitators/api/urls.py
@@ -6,7 +6,7 @@ from facilitators.api import views
 
 urlpatterns = [
     path('facilitator/api/createcourse/', CreateCourseApi.as_view(), name='createcourseapi'),
-    path('facilitator/api/dashboard/explore', views.courses, name="course"),
+    path('facilitator/api/dashboard/explore', views.courses, name="course1"),
     path('facilitator/api/support',views.support,name='support')
     #path('api/login/', LoginAPI.as_view(), name='login'),
     # path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
