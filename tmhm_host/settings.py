0e8b6b8954135326e29c55536a55ac31c80aa532
Vijay gwala <55628967+vijaygwala@users.noreply.github.com>
2020-10-10 11:46:53 +0530

    staged

--- tmhm_host/settings.py
+++ tmhm_host/settings.py
@@ -244,7 +244,7 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
 #By vijay
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = 1
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'learnopad@gmail.com'
 EMAIL_HOST_PASSWORD = 'learnOpad@2020'
